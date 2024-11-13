196a987ddc2fa12079fbd2ce0c0cc3825cd5ef82
GriffinSSloves <griffinsloves@gmail.com>
2024-11-12 17:47:52 -0800

    Improve config and readme

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: '/', // Since you're deploying to the root
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        copyPublicDir: true
+    }
 })
