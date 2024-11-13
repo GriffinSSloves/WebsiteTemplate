cd67fa2b13759d9f7be7c81b6ec9752dc2d7aede
GriffinSSloves <griffinsloves@gmail.com>
2024-11-12 17:15:56 -0800

    Ran prettier on all files, simplifying the build a bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-
 import './App.css'
 import './index.css'
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <header className="bg-white shadow rounded-lg p-6 mb-8">
-        <h1 className="text-3xl font-bold text-gray-900">
-          My Website
-        </h1>
-      </header>
-      <main className="max-w-4xl mx-auto">
-        {/* Your content here */}
-      </main>
-    </div>
-  )
+    return (
+        <div className='min-h-screen bg-gray-100 p-4'>
+            <header className='bg-white shadow rounded-lg p-6 mb-8'>
+                <h1 className='text-3xl font-bold text-gray-900'>My Website</h1>
+            </header>
+            <main className='max-w-4xl mx-auto'>{/* Your content here */}</main>
+        </div>
+    )
 }
 
 export default App
