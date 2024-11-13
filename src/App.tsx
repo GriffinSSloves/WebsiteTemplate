7629f055a829b88af3dfc389ac915ee45feddcb8
GriffinSSloves <griffinsloves@gmail.com>
2024-11-12 16:50:27 -0800

    Vite configuration done + added tailwind for css

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <header className="bg-white shadow rounded-lg p-6 mb-8">
+        <h1 className="text-3xl font-bold text-gray-900">
+          My Website
+        </h1>
+      </header>
+      <main className="max-w-4xl mx-auto">
+        {/* Your content here */}
+      </main>
+    </div>
+  )
+}
+
+export default App
